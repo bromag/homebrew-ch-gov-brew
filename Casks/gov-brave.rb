b7ae7b03bba1a601978c3342accb88ed6f9daa1c
Romagnoli Giuliano BIT <giuliano.romagnoli@bit.admin.ch>
2025-06-26 15:41:17 +0200

    initial commit

---
+++ Casks/gov-brave.rb
@@ -0,0 +1,37 @@
+cask "gov-brave-browser" do
+  arch arm: "arm64", intel: "x64"
+  folder = on_arch_conditional arm: "stable-arm64", intel: "stable"
+
+  version "1.79.126.0"
+  sha256 arm:   "5e6abcfd6cfc3eac27eb436113dea5d729f39bddd1c947c048f9b3722c7707ff",
+         intel: "c6acf94ac9af5bd1d28ee3bb8c2c0f8c8eac8252eb447ddf8ca8bf8ba0826a86"
+
+  url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.major_minor_patch.sub(".", "")}/Brave-Browser-#{arch}.dmg",
+      verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
+  name "Brave"
+  desc "Web browser focusing on privacy"
+  homepage "https://brave.com/"
+
+  livecheck do
+    url "https://updates.bravesoftware.com/sparkle/Brave-Browser/#{folder}/appcast.xml"
+    strategy :sparkle, &:short_version
+  end
+
+  auto_updates true
+  depends_on macos: ">= :big_sur"
+
+  app "Brave Browser.app"
+
+  zap trash: [
+        "~/Library/Application Support/BraveSoftware/Brave-Browser",
+        "~/Library/Caches/BraveSoftware/Brave-Browser",
+        "~/Library/Caches/com.brave.Browser",
+        "~/Library/HTTPStorages/com.brave.Browser",
+        "~/Library/Preferences/com.brave.Browser.plist",
+        "~/Library/Saved Application State/com.brave.Browser.savedState",
+      ],
+      rmdir: [
+        "~/Library/Application Support/BraveSoftware",
+        "~/Library/Caches/BraveSoftware",
+      ]
+end
