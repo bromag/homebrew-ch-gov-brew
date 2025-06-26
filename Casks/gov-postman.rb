d341621c4f10cd3b6b2947380e86f90ad4010134
Romagnoli Giuliano BIT <giuliano.romagnoli@bit.admin.ch>
2025-06-26 13:33:13 +0200

    initial commit

---
+++ Casks/gov-postman.rb
@@ -0,0 +1,41 @@
+cask "govpostman" do
+  arch arm: "osx_arm64", intel: "osx64"
+
+  version "11.50.5"
+  sha256 arm:   "28ebd8b7754bd61640c9fe93fb7c21313ca11e09dd9e564ed1edcbd4ab43c6a1",
+         intel: "2c2a716c381c21b56603b458ca4962ee02c66f41564382e283182165812fd419"
+
+  url "https://dl.pstmn.io/download/version/#{version}/#{arch}",
+      verified: "dl.pstmn.io/download/version/"
+  name "Postman"
+  desc "Collaboration platform for API development"
+  homepage "https://www.postman.com/"
+
+  # This is a workaround to a slow-to-update livecheck. It uses the in-app
+  # update check link and queries the available versions for a generic major
+  # version. We cannot use #{version} as the URL does not exist if #{version}
+  # is the latest version available.
+  livecheck do
+    url "https://dl.pstmn.io/update/status?currentVersion=#{version.major}.0.0&platform=#{arch}"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Postman.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.postmanlabs.mac.ShipIt",
+    "~/Library/Application Support/Postman",
+    "~/Library/Caches/com.postmanlabs.mac",
+    "~/Library/Caches/com.postmanlabs.mac.ShipIt",
+    "~/Library/Caches/Postman",
+    "~/Library/HTTPStorages/com.postmanlabs.mac",
+    "~/Library/Preferences/ByHost/com.postmanlabs.mac.ShipIt.*.plist",
+    "~/Library/Preferences/com.postmanlabs.mac.plist",
+    "~/Library/Saved Application State/com.postmanlabs.mac.savedState",
+  ]
+end
